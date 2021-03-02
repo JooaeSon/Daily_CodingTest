855453b285af70028040198303dcdfcc88db9143
JooaeSon <9apples1718@gmail.com>
2021-03-02 18:14:44 +0900

    2021.03.02.

--- SWEA/동적 계획법의 소개/타일 붙이기.py
+++ SWEA/동적 계획법의 소개/타일 붙이기.py
@@ -9,7 +9,6 @@ for test_case in range(1, T + 1):
     memo[1], memo[2], memo[3] = 1, 3, 6
 
     for i in range(4, N+1):
-        if memo[i] == 0:
-            memo[i] = memo[i-1]+2*memo[i-2]+memo[i-3]
+        memo[i] = memo[i-1]+2*memo[i-2]+memo[i-3]
 
     print(f'#{test_case} {memo[N]}')
