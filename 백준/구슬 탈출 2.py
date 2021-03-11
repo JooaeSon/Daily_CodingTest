ddd8a8fa7630da6d692a13372250da31a09bdc4b
JooaeSon <9apples1718@gmail.com>
2021-03-11 22:57:29 +0900

    2021.03.11.

--- 백준/구슬 탈출 2.py
+++ 백준/구슬 탈출 2.py
@@ -1,9 +1,6 @@
 from sys import stdin
 from collections import deque
 
-dx = [0, -1, 0, 1]
-dy = [1, 0, -1, 0]
-
 N, M = map(int, stdin.readline().split())
 board = [list(stdin.readline()) for _ in range(N)]
 
@@ -17,17 +14,17 @@ for i in range(N):
             board[i][j] = '.'
 
 
-def movemove(y, x, ddy, ddx):
+def movemove(x, y, ddx, ddy):
     move = 0
 
-    while board[y + ddy][x + ddx] != '#':
-        if board[y+ddy][x+ddx] == 'O': # 구멍으로 탈출 할 경우
+    while board[x + ddx][y + ddy] != '#':
+        if board[x+ddx][y+ddy] == 'O': # 구멍으로 탈출 할 경우
             return 0, 0, 0
-        y += ddy
         x += ddx
+        y += ddy
         move += 1
 
-    return y, x, move
+    return x, y, move
 
 
 def BFS():
@@ -36,38 +33,38 @@ def BFS():
     visit[red[0], red[1], blue[0], blue[1]] = 0
 
     while deq:
-        ry, rx, by, bx = deq.popleft()
+        rx, ry, bx, by = deq.popleft()
 
-        for k in range(4):
+        for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
             # 한쪽 방향으로 계속 움직일 수 있을 때까지 움직인다.
-            nry, nrx, rmove = movemove(ry, rx, dy[k], dx[k])
-            nby, nbx, bmove = movemove(by, bx, dy[k], dx[k])
+            nrx, nry, rmove = movemove(rx, ry, dx, dy)
+            nbx, nby, bmove = movemove(bx, by, dx, dy)
 
             # 두 공 모두 또는 파란 공만 탈출한 경우
             if not nbx and not nby:
                 continue
             # 빨간 공만 탈출한 경우
             elif not nrx and not nry:
-                print(visit[ry, rx, by, bx]+1)
+                print(visit[rx, ry, bx, by]+1)
                 return
             # 두 공이 같은 위치에 있는 경우
-            elif nrx == nbx and nry==nby:
+            elif nrx == nbx and nry == nby:
                 # 이동거리가 적은 구슬을 한 칸 뒤로
                 if rmove > bmove:
-                    nrx -= dx[k]
-                    nry -= dy[k]
+                    nrx -= dx
+                    nry -= dy
                 else:
-                    nbx -= dx[k]
-                    nby -= dy[k]
+                    nbx -= dx
+                    nby -= dy
             # visit하지 않았으면 queue에 append
-            if (nry, nrx, nby, nbx) not in visit:
-                visit[nry, nrx, nby, nbx] = visit[ry, rx, by, bx]+1
-                deq.append([nry, nrx, nby, nbx])
+            if (nrx, nry, nbx, nby) not in visit:
+                visit[nrx, nry, nbx, nby] = visit[rx, ry, bx, by]+1
+                deq.append([nrx, nry, nbx, nby])
 
-    # answer에 값을 넣었거나 deq이 비었거나 움직인 횟수가 10 이상일때
-    if not deq or visit[ry, rx, by, bx] >= 10:
-        print(-1)
-        return
+        # answer에 값을 넣었거나 deq이 비었거나 움직인 횟수가 10 이상일때
+        if not deq or visit[rx, ry, bx, by] >= 10:
+            print(-1)
+            return
 
 BFS()
 
