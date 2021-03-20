d7911534880c6f35a2dbbd225dfb94b838a0c45f
JooaeSon <9apples1718@gmail.com>
2021-03-20 15:32:52 +0900

    2021.03.20.

--- 백준/연산자 끼워넣기.py
+++ 백준/연산자 끼워넣기.py
@@ -1,34 +1,33 @@
 from sys import stdin
 
 N = int(stdin.readline())
-A = [map(int, stdin.readline().split())]
-O = [map(int, stdin.readline().split())]
-N_operator = sum(O) # 연산자 총 개수
+A = list(map(int, stdin.readline().split()))
+a, b, c, d = map(int, stdin.readline().split())
 
 
-def dfs(cnt):
-    global res, Max, Min
+def cal(num, idx, plus, subtract, multiply, division):
+    global N, Max, Min
 
-    if not -1000000000<=res<=1000000000:
+    if idx==N:
+        Max=max(num, Max)
+        Min=min(num, Min)
         return
-
-    if cnt >= N_operator-1:
-        if res > Max: # 최대값 찾기
-            Max=res
-        if res < Min: # 최소값 찾기
-            Min=res
-        return
-
-    for i in range(len(A)):
-        dfs(cnt+1)
-    return
+    else:
+        if plus:
+            cal(num+A[idx], idx+1, plus-1, subtract, multiply, division)
+        if subtract:
+            cal(num - A[idx], idx + 1, plus, subtract-1, multiply, division)
+        if multiply:
+            cal(num*A[idx], idx+1, plus, subtract, multiply-1, division)
+        if division:
+            cal(int(num / A[idx]), idx + 1, plus, subtract, multiply, division-1)
 
 
-res = 0
 Max = float('-inf')
 Min = float('inf')
 
-dfs(0)
+
+cal(A[0], 1, a, b, c, d)
 
 print(Max)
 print(Min)
