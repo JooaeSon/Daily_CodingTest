9d2e3e6e7fd0f55fe8970f795a5372df9669cfb9
JooaeSon <9apples1718@gmail.com>
2021-04-12 23:29:24 +0900

    2021.04.12.

--- 백준/게리맨더링 2.py
+++ 백준/게리맨더링 2.py
@@ -1,3 +1,26 @@
+
+
+def divide(x, y, d1, d2, answer):
+    while True:
+        while True:
+            lx, ly, rx, ry = x+d1, y-d1, x+d2, y+d2
+            if rx == N-1 or ry ==N:
+                break
+    return answer
+
 N = int(input())
-A = [list(map(int, input().split())) for _ in range(N)]
+
+a, nsum = [], 0
+for _ in range(N):
+    row = list(map(int, input().split()))
+    nsum += sum(row)
+    a.append(row)
+
+answer=float('inf')
+for i in range(N-2):
+    for j in range(1, N-1):
+        d1, d2 = 1, 1
+        answer = divide(i, j, d1, d2, answer)
+
+print(answer)
 
