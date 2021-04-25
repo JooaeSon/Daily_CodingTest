1f45fed1b8a8dface309f06b8d06cd0d8ac8991b
JooaeSon <9apples1718@gmail.com>
2021-04-25 23:26:11 +0900

    2021.04.25.

--- 백준/마법사 상어와 토네이도.py
+++ 백준/마법사 상어와 토네이도.py
@@ -7,10 +7,26 @@ dy=[-1, 0, 1, 0]
 
 x, y = N//2, N//2
 ans=0
+dir, step = 0, 0
 while True:
 
-    if x==0 and y==0:
+    if dir==0 or dir==2:
+        step+=1
+
+    if not 0<=x<N or not 0<=y<N:
         break
+
+    cnt=0
+    while cnt<step:
+        nx, ny = x+dx[dir], y+dy[dir]
+        print(nx, ny)
+        cnt+=1
+        x, y=nx, ny
+
+
+
+    dir = (dir+1)%4
+
 
 
 
