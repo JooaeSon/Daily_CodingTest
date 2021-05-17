8ece54ec464bc1658342d59df2b1a6a1f697b896
JooaeSon <9apples1718@gmail.com>
2021-05-17 23:08:04 +0900

    2021.05.17.

--- Programmers/Lv.2/2개 이하로 다른 비트.py
+++ Programmers/Lv.2/2개 이하로 다른 비트.py
@@ -1,43 +1,17 @@
-def findDifferMinium(num):
-    tmp = num + 1
+def f(n):
+    bit_length = n.bit_length()
 
-    while True:
-        binNum, binTmp = bin(num)[2:], bin(tmp)[2:]
-        s_binNum, s_binTmp = str(binNum), str(binTmp)
-
-        if len(s_binNum)>len(s_binTmp):
-            maxlength=len(s_binNum)
-            s_binTmp='0'*(len(s_binNum)-len(s_binTmp))+s_binTmp
-        else:
-            maxlength = len(s_binTmp)
-            s_binNum = '0' * (len(s_binTmp) - len(s_binNum)) + s_binNum
-
-        flag = True
-        cnt = 0
-        i = -1
-        while True:
-            if s_binNum[i] != s_binTmp[i]:
-                cnt += 1
-
-            if cnt > 2:
-                flag = False
-                break
-
-            if maxlength==abs(i):
-                break
-            i-=1
-
-        if flag:
-            return tmp
-        tmp += 1
-
+    if not n & 1:
+        return n | 1
+    if not (n+1) & n:
+        return (n | (n+1)) ^ (1 << (bit_length-1))
+    for i in range(bit_length):
+        if (1 << i) | n != n:
+            n |= (1 << i)
+            n ^= (1 << (i-1))
+            return n
 
 def solution(numbers):
-    answer = []
+    return [f(x) for x in numbers]
 
-    for number in numbers:
-        answer.append(findDifferMinium(number))
-    print(answer)
-    return answer
-
-solution([2,7])
+# 참조: https://blog.hoony.me/3
