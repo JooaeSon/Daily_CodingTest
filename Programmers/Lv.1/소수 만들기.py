1d062377b4759c63238e1a5a6077cc2984d70d22
JooaeSon <9apples1718@gmail.com>
2021-06-03 06:28:05 +0900

    2021.06.03.

---
+++ Programmers/Lv.1/소수 만들기.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+
+
+def is_prime_number(x):
+    # 2부터 (x - 1)까지의 모든 수를 확인
+    for i in range(2, x):
+        # x가 해당 수로 나누어떨어지면
+        if x % i == 0:
+            return False
+    return True
+
+
+def solution(nums):
+    answer=0
+    lst=list(combinations(nums, 3))
+
+    for l in lst:
+        if is_prime_number(sum(l)):
+            answer+=1
+
+    return answer
