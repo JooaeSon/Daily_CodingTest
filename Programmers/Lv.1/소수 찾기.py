f0a5baa7da46765873dd15645ed0e82bfedbea06
JooaeSon <9apples1718@gmail.com>
2021-06-16 22:36:43 +0900

    2021.06.16.

---
+++ Programmers/Lv.1/소수 찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * (n+1)
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int((n+1) ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n+1, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return len([i for i in range(2, n+1) if sieve[i] == True])
