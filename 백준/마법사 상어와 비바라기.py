91d7f9359ec3ba781d7a835344d9a3279df58062
JooaeSon <9apples1718@gmail.com>
2021-06-23 00:36:19 +0900

    2021.06.23.

--- 백준/마법사 상어와 비바라기.py
+++ 백준/마법사 상어와 비바라기.py
@@ -1,4 +1,24 @@
-N, M = map(int, input().split())
+import copy
 
+N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 
+dx = [0, -1, -1, -1, 0, 1, 1, 1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+
+infos = [list(map(int, input().split())) for _ in range(M)]
+cloud = [[0 for _ in range(N)] for _ in range(N)]
+cloud[N-1][0], cloud[N-1][1], cloud[N-2][0], cloud[N-2][1] = 1, 1, 1, 1
+
+ans = 0
+
+for info in infos:
+    d, s = infos
+
+    for i in range(N):
+        for j in range(N):
+            if cloud[i][j]==1:
+                nx, ny = i+(s*dx[d])%N, j+(s*dy[d])%N
+
+
+print(ans)
