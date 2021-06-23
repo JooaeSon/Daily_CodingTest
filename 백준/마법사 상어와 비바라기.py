85865540feb167357db76a6a5cd03fdd926951f8
JooaeSon <9apples1718@gmail.com>
2021-06-24 01:07:09 +0900

    2021.06.24.

--- 백준/마법사 상어와 비바라기.py
+++ 백준/마법사 상어와 비바라기.py
@@ -13,12 +13,25 @@ cloud[N-1][0], cloud[N-1][1], cloud[N-2][0], cloud[N-2][1] = 1, 1, 1, 1
 ans = 0
 
 for info in infos:
-    d, s = infos
+    d, s = info
+    d-=1
+    tmp_cloud = [[0 for _ in range(N)] for _ in range(N)]
 
     for i in range(N):
         for j in range(N):
             if cloud[i][j]==1:
-                nx, ny = i+(s*dx[d])%N, j+(s*dy[d])%N
+                nx, ny = (i+s*dx[d])%N, (j+s*dy[d])%N
+                tmp_cloud[nx][ny]=1
+
+    cloud=tmp_cloud
+
+    for i in range(N):
+        for j in range(N):
+            if cloud[i][j]==1:
+                A[i][j]+=1
+
+
+
 
 
 print(ans)
