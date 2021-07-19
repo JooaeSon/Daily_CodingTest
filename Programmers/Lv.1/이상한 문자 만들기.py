6eadd53fa7b39bd5b9b9de47931dd822bd5c8522
JooaeSon <9apples1718@gmail.com>
2021-07-19 20:56:56 +0900

    2021.07.19.

--- Programmers/Lv.1/이상한 문자 만들기.py
+++ Programmers/Lv.1/이상한 문자 만들기.py
@@ -1,1 +1,13 @@
-yy
+def solution(s):
+    answer = ''
+
+    for word in list(s.split(" ")):
+        for i in range(len(word)):
+            if i % 2:
+                answer += word[i].lower()
+            else:
+                answer += word[i].upper()
+
+        answer += ' '
+
+    return answer[:-1]
