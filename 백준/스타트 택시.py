26001a14561f83deac4a38b6df655f4ca46a4f64
JooaeSon <9apples1718@gmail.com>
2021-07-27 00:20:06 +0900

    2021.07.27.

--- 백준/스타트 택시.py
+++ 백준/스타트 택시.py
@@ -8,44 +8,49 @@ taxi[0]-=1
 taxi[1]-=1
 
 # 고객 정보를 담기 위한
-customer_info=[list(map(int, input().split())) for _ in range(M)]
-
+customers=[list(map(int, input().split())) for _ in range(M)]
 
 dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
 
-
-def findCumtomer(): # bfs를 사용하여 고객 찾기
+# 손님 찾기
+def findCustomer(customer): # bfs를 사용하여 고객 찾기
+    global F
     x, y = taxi
     dist=0
-    deq=deque([[x, y, dist]])
+
+    deq=deque([(x, y, dist)])
     visited = copy.deepcopy(board)
-    visited[x][y]=-1
+    visited[x][y] = -1
+
+    cx, cy, px, py = customer[0], customer[1], customer[2], customer[3]
 
     # 가장 가까운 고객 지점 찾기
-    result=[]
     while deq:
         x, y, dist= deq.popleft()
         print(x, y)
-        dist+=1
-        print(dist)
-        if customer_info[x][y] and customer_info[x][y][0]==1:# 고객 출발 지점이라면
-            result.append(customer_info[x][y])
-            customer_info[x][y] = [] # 이미 택시 탄 고객은 좌표에서 초기화
+        if cx==x and cy==y:
+            F-=dist
             break
-        for dir in range(4):
+
+        for dir in range(4): # 상하좌우 탐색
             nx, ny = x+dx[dir], y+dy[dir]
-            if 0<=nx<N and 0<=ny<N and not board[nx][ny] and not visited[nx][ny]:
+            if 0<=nx<N and 0<=ny<N and not board[nx][ny] and not visited[nx][ny]: # 가는 길이 빈칸인지 아직 방문하지 않은 곳인지 확인
                 visited[nx][ny]=-1
-                deq.append([nx, ny, dist])
+                deq.append((nx, ny, dist+1))
                 x, y = nx, ny
 
-    return dist, result
+    return
 
+# 손님 데려다주기
+def moveCustomer(customer):
+    global F
+    cx, cy, px, py = customer[0], customer[1], customer[2], customer[3]
 
-for _ in range(M):
-    dist, customer = findCumtomer()
-    print(customer)
-    print("dist:", dist)
+    return
+
+for i in range(M):
+    findCustomer(customers[i])
+    moveCustomer(customers[i])
 
 print(F)
