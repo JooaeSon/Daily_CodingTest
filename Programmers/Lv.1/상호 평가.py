aaaf49b7e5fedff10be3a80f62f71b10049b8c92
JooaeSon <9apples1718@gmail.com>
2021-08-09 15:55:03 +0900

    2021.08.09.

--- Programmers/Lv.1/상호 평가.py
+++ Programmers/Lv.1/상호 평가.py
@@ -16,8 +16,7 @@ def solution(scores):
     scores = list(map(list, zip(*scores)))
 
     for i in range(len(scores)):
-        if (scores[i][i] == max(scores[i]) and scores[i].count(max(scores[i])) == 1) or (
-                scores[i][i] == min(scores[i]) and scores[i].count(min(scores[i])) == 1):
+        if (scores[i][i] == max(scores[i]) and scores[i].count(max(scores[i])) == 1) or (scores[i][i] == min(scores[i]) and scores[i].count(min(scores[i])) == 1):
             scores[i][i] = 0
 
     idx = 0
