d35bf4aea6873c82fb21578724a6d0871fec9d45
JooaeSon <9apples1718@gmail.com>
2021-08-12 20:44:21 +0900

    2021.08.12.

--- Programmers/Lv.2/행렬 테두리 회전하기.py
+++ Programmers/Lv.2/행렬 테두리 회전하기.py
@@ -1,9 +1,18 @@
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
 
 def rotate(query):
     x1, y1, x2, y2 = query
+    sx, sy, ex, ey = x1 - 1, y1 - 1, x2 - 1, y2 - 1
+
+    curr_x, curr_y = sx, sy
+    for dir in range(4):
+        while True:
+            nx, ny = curr_x + dx[dir], curr_y + dy[dir]
+            if not sx <= nx <= ex or not sy <= ny <= ey:
+                break
+            curr_x, curr_y = nx, ny
 
     return
 
