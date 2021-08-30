b2effe00224a17939d3ba8c8c6cc74544b660815
JooaeSon <9apples1718@gmail.com>
2021-08-31 00:21:45 +0900

    2021.08.31.

--- Programmers/Lv.1/직업군 추천하기.py
+++ Programmers/Lv.1/직업군 추천하기.py
@@ -1,16 +1,16 @@
 def solution(table, languages, preference):
-    answer = ''
+    answer = []
 
     for row in table:
         Sum = 0
         stream = list(row.split())
-        print(stream)
+
         jobs = stream[0]
         for i in range(len(stream[1:])):
             if stream[1:][i] in languages:
-                print(stream[1:][i])
                 idx = languages.index(stream[1:][i])
-                Sum += preference[idx] * (len(stream[1:]) - 1)
-        print(Sum)
+                Sum += preference[idx] * (len(stream[1:]) - i)
 
-    return answer
+        answer.append((jobs, Sum))
+
+    return sorted(answer, key=lambda x: (-x[1], x[0]))[0][0]
