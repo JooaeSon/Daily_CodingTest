424eedd8e39b076f949ba9a38bee1b919473e8f9
JooaeSon <9apples1718@gmail.com>
2021-09-07 00:57:41 +0900

    2021.09.07.

--- 백준/상어 초등학교.py
+++ 백준/상어 초등학교.py
@@ -6,15 +6,30 @@ dict = defaultdict(list)
 classRoom=[[0 for _ in range(N)] for _ in range(N)]
 ans=0
 
+dx=[1, 0, -1, 0]
+dy=[0, -1, 0, 1]
+
 for i in range(len(lst)):
     dict[lst[i][0]].extend(lst[i][1:])
 
 print(dict)     
 
+# 가장많은 빈칸인 것 찾기
+for i in range(N):
+    for j in range(N):
+        cnt=0
+        if classRoom[i][j]==0:
+            for dir in range(4):
+                nx, ny=i+dx[dir], j+dy[dir]
+                if classRoom[nx][ny]==0:
+                    cnt+=1
+
+
+
 
 # 만족도 구하기
-for i in range(len(classRoom)):
-    for j in range(len(classRoom)):
+for i in range(N):
+    for j in range(N):
         classRoom[i][j]
 
 print(ans)
