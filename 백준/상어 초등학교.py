2113970a5bcb75498b471d451935ac977b232a16
JooaeSon <9apples1718@gmail.com>
2021-09-28 23:24:37 +0900

    2021.09.28.

--- 백준/상어 초등학교.py
+++ 백준/상어 초등학교.py
@@ -17,11 +17,8 @@ dy=[0, -1, 0, 1]
 for i in range(len(lst)):
     dict[lst[i][0]].extend(lst[i][1:])
 
-print(dict)     
 
 for std, like_std in dict.items():
-    print("std:", std)
-    print("like_std: ", like_std)
     # 인접한 칸에 좋아하는 학생들이 많은 칸 고르기
     seat = []
     for i in range(N):
@@ -41,16 +38,25 @@ for std, like_std in dict.items():
     # 좋아하는 사람 수(내림), 빈칸 수(내림), 행(오름), 렬(오름) 순서로 정렬한다.
     seat=sorted(seat, key=lambda x: (-x[0], -x[1], x[2], x[3]))
     classRoom[seat[0][2]][seat[0][3]]=std
-    print(seat)
-    print("classRoom: ", classRoom)
-    # 만족도 구하기
-    satisfaction=0
-    if seat[0][0]==0: satisfaction=0
-    elif seat[0][0]==1: satisfaction=1
-    elif seat[0][0]==2: satisfaction=10
-    elif seat[0][0] == 3: satisfaction = 100
-    else: satisfaction=1000
-    print("satisfaction: ", satisfaction)
-    ans+=satisfaction
+
+
+# 만족도 구하기
+for i in range(N):
+    for j in range(N):
+        cnt=0
+
+        for dir in range(4):  # 사방을 탐색하여 좋아하는 학생이 있는지 찾는다.
+            nx, ny = i + dx[dir], j + dy[dir]
+            if 0 <= nx < N and 0 <= ny < N:
+                if classRoom[nx][ny] in dict[classRoom[i][j]]:
+                    cnt+=1
+
+        if cnt==0: satisfaction=0
+        elif cnt==1: satisfaction=1
+        elif cnt==2: satisfaction=10
+        elif cnt == 3: satisfaction = 100
+        else: satisfaction=1000
+
+        ans+=satisfaction
 
 print(ans)
