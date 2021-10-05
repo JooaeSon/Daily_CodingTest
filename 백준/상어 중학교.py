674036b3b1e6e9ba41b30c8b8821a54979a29b6f
JooaeSon <9apples1718@gmail.com>
2021-10-05 22:57:04 +0900

    2021.10.05.

--- 백준/상어 중학교.py
+++ 백준/상어 중학교.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import *
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
@@ -13,12 +13,14 @@ def isInGroup(): # 격자에 그룹이 존재하는지 확인
     return True
 
 
-def bfs(x, y):
+def bfs(x, y, area, dic):
     # 기준 x, y
     deq = deque([(x, y)])
+
     cnt, rainbow =0, 0
     while deq:
         x, y=deq.popleft()
+        dic[(x, y)]=[(x, y)] # 기준 시작 좌표에 해당하는 경로 모두 담기 위해
         for dir in range(4):
             nx, ny = x+dx[dir], y+dy[dir]
             # 범위 안에 있고 아직 방문하지 않았어야 했고 검은블록(-1) 이 아니여야 한다.
@@ -26,19 +28,22 @@ def bfs(x, y):
                 if board[nx][ny]==0: # 무지개 블록 갯수
                     rainbow+=1
                 deq.append((nx, ny))
+                dic[(x, y)].append((nx, ny)) # 지나간 좌표 표시/그룹에 속한 좌표 나중에 찾으려고
                 visited[nx][ny] = -1
                 cnt+=1 # 전체 블록 갯수
 
-    # 가장 큰 블록 > 무지개 블록 수 > 행 번호 > 열 번호
+    # 블록 수 > 무지개 블록 수 > 행 번호 > 열 번호
     area.append((cnt, rainbow, x, y))
     
-    return
+    return dic, area
 
 
 while True:
     # 방문 블록 초기화
     visited = [[0 for _ in range(N)] for _ in range(N)]
-    area = [] # 블록 그룹 정보 초기화
+    dic = {}
+    area = []  # 블록 그룹 정보 초기화
+
     # 격자에 그룹이 존재하는지 확인하기
     if not isInGroup():
         break
@@ -47,8 +52,9 @@ while True:
     for i in range(N):
         for j in range(N):
             if not visited[i][j] and board[i][j]!=-1:
-                bfs(i, j)
-                
+                dic, area=bfs(i, j, area, dic)
+    area=sorted(area, key=lambda x:(-x[0], -x[1], -x[2], -x[3]))
+    rmove_block_lst=dic[(area[0][2], area[0][3])]
     # 2. 1에서 찾은 블록 그룹 모두 제거, 블록수^2만큼 점수 획득
     # 3. 격자에 중력이 작용
     # 4. 격자가 90도 반시계 방향으로 회전
