efa7b466835f736af731367309b17fd4e532d4ac
JooaeSon <9apples1718@gmail.com>
2021-10-06 23:45:38 +0900

    2021.10.06.

--- 백준/상어 중학교.py
+++ 백준/상어 중학교.py
@@ -9,6 +9,13 @@ dy = [0, 1, 0, -1]
 
 
 def isInGroup(): # 격자에 그룹이 존재하는지 확인
+    '''
+    그룹에는 일반 블록이 적어도 하나 있어야 하며, 일반 블록의 색은 모두 같아야 한다.
+    검은색 블록은 포함되면 안 되고, 무지개 블록은 얼마나 들어있든 상관없다.
+    그룹에 속한 블록의 개수는 2보다 크거나 같아야 하며,
+    임의의 한 블록에서 그룹에 속한 인접한 칸으로 이동해서 그룹에 속한 다른 모든 칸으로 이동할 수 있어야 한다.
+    '''
+
 
     return True
 
@@ -51,11 +58,17 @@ while True:
     # 1. 크기가 가장큰 블록 그룹 찾기, area정렬 순서에 따라 가장 앞에 있는 요소 가져오기
     for i in range(N):
         for j in range(N):
-            if not visited[i][j] and board[i][j]!=-1:
+            if not visited[i][j] and board[i][j]!=-1 and board[i][j] != 0:
                 dic, area=bfs(i, j, area, dic)
     area=sorted(area, key=lambda x:(-x[0], -x[1], -x[2], -x[3]))
     rmove_block_lst=dic[(area[0][2], area[0][3])]
-    # 2. 1에서 찾은 블록 그룹 모두 제거, 블록수^2만큼 점수 획득
+
+    # 2. 1에서 찾은 블록 그룹 모두 제거
+    for x, y in rmove_block_lst:
+        board[x][y] = ''
+
+    ans+=len(rmove_block_lst)**2 # 블록수^2만큼 점수 획득
+
     # 3. 격자에 중력이 작용
     # 4. 격자가 90도 반시계 방향으로 회전
     # 5. 다시 격자에 중력이 작용
