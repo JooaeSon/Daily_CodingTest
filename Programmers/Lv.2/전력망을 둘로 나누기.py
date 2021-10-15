241cea528bbcf046edb89d5c19478cc78b4184fa
JooaeSon <9apples1718@gmail.com>
2021-10-16 00:20:27 +0900

    2021.10.16.

---
+++ Programmers/Lv.2/전력망을 둘로 나누기.py
@@ -0,0 +1,10 @@
+def solution(n, wires):
+    answer = -1
+
+    for wire in wires:
+        a, b = wire
+        a_set, b_set = set(), set()
+        a_set.add(a)
+        b_set.add(b)
+
+    return answer
