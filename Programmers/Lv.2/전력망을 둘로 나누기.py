028fa4b404a401f0b632588c44e8f083cfc010f5
JooaeSon <9apples1718@gmail.com>
2021-10-17 01:22:28 +0900

    2021.10.17.

--- Programmers/Lv.2/전력망을 둘로 나누기.py
+++ Programmers/Lv.2/전력망을 둘로 나누기.py
@@ -1,10 +1,30 @@
+from collections import defaultdict
+from collections import deque
+
+
+def bfs(wire, adj):
+    deq = deque()
+
+    while deq:
+        n1 = deq.popleft()
+        for n2 in adj[n1]:
+            deq.append(n2)
+
+    return
+
+
 def solution(n, wires):
     answer = -1
 
+    adj = defaultdict(list)
+
     for wire in wires:
-        a, b = wire
-        a_set, b_set = set(), set()
-        a_set.add(a)
-        b_set.add(b)
+        adj[wire[0]].append(wire[1])
+        adj[wire[1]].append(wire[0])
+
+    print(adj)
+
+    for wire in wires:
+        bfs(wire, adj)
 
     return answer
