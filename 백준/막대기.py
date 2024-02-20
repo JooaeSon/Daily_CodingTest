cd792389e54f2156f0e074f8d6df3476196ebf64
JooaeSon <9apples1718@gmail.com>
2024-02-21 00:39:53 +0900

    2024.02.21

--- 백준/막대기.py
+++ 백준/막대기.py
@@ -2,11 +2,11 @@ X = int(input())
 Stick=64
 ans=0
 
-while True:
-    if Stick<=X:
-        break
-    Stick=Stick//2
-    Stick+=X
-    ans+=1
+while X>0:
+    if Stick > X:
+        Stick=Stick//2
+    else:
+        X-=Stick
+        ans+=1
 
 print(ans)
